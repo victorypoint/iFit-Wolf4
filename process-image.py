205823308058aa03d0b3113eb5696e5c50820bdd
Al Udell <63697253+victorypoint@users.noreply.github.com>
2023-05-01 20:35:55 -0300

    Add files via upload

--- process-image.py
+++ process-image.py
@@ -29,7 +29,7 @@ screenshot_np = np.array(screenshot)
 # Convert numpy array to a cv2 RGB image
 screenshot_cv2 = cv2.cvtColor(screenshot_np, cv2.COLOR_BGR2RGB)
 
-# Crop image to incline area
+# Crop image to workout instruction area
 screenwidth, screenheight = screenshot.size
 col1 = int(screenwidth/3000 * 1010)
 row1 = int(screenheight/2000 * 260)
@@ -52,7 +52,6 @@ num_pattern = r'\d+(\.\d+)?'  # Regular expression pattern to match numbers with
 unit_pattern = r'\s+(kph|mph)'  # Regular expression pattern to match "kph" or "mph" units
 speed_match = re.search(num_pattern + unit_pattern, ocr_text)
 if speed_match:
-    #speed = float(speed_match.group(0))
     speed = speed_match.group(0)
     pattern = r'\d+\.\d+'
     speed = re.findall(pattern, speed)[0]
@@ -63,7 +62,6 @@ else:
 incline_pattern = r'\d+\s*%'  # Regular expression pattern to match numbers with "%"
 incline_match = re.search(incline_pattern, ocr_text)
 if incline_match:
-    #incline = float(incline_match.group(0))
     incline = incline_match.group(0)
     pattern = r'\d+'
     incline = re.findall(pattern, incline)[0]
